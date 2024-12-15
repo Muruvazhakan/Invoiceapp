a8438b8c57d919efc1870b7b8b4a1ff94d5a3232
muruvazhakan <puhalmuru@gmail.com>
2024-12-15 22:27:39 +0530

    add image in invoice

--- src/Component/Screen/GeneralDetails/GeneralDetails.js
+++ src/Component/Screen/GeneralDetails/GeneralDetails.js
@@ -1,66 +1,118 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 
 
 import './GeneralDetails.css';
 import { AllState } from "../../Context/allStateContext";
-const GeneralDetails = () =>{
+import { CompanyDetail } from "../../Context/companyDetailContext";
+const GeneralDetails = () => {
 
-    const invoicedet=useContext(AllState);
-    return <div className="generaldetails">
-        <ul className="details">
-            <div className="companyname">Buyer:
-            <li> 
-                {invoicedet.clientName}               
-            </li>
-            <li> 
-                {invoicedet.clientPhno}               
-            </li>
-            <li>
-                {invoicedet.clientAdd} 
-            </li>
-            </div>
-        </ul>
-       
-        <ul className="details">
-            <li className="companyname">
-                {invoicedet.companyName}
-            </li>
-            <li>
-            {invoicedet.companyTagLine}
-            </li>
-            <li>
-            {invoicedet.companyAddress}
-            </li>
-            <li>
-            {invoicedet.companyName}
-            </li>
-            <li>
-            {invoicedet.companyPhno}
-            </li>
-            <li>
-             {invoicedet.companyGstin ? 'Gstin: '+invoicedet.companyGstin:null }
-            </li>
-            <li>
-             {invoicedet.companyGstinStatename ? 'StateName: '+invoicedet.companyGstinStatename:null }
-            </li>
+    const invoicedet = useContext(AllState);
+    const companydet = useContext(CompanyDetail);
+    return <>
 
-        </ul>
-       
-        <ul className="details invoicedetails">
-            <li>
-            Invoice ID: {invoicedet.invoiceid}
+        <div className="generaldetails ">
+            <ul className="details ">
+                <li >
+                    <div className="companyname big estimateheader">
+                        {companydet.companyName}
+
+                    </div>
+                </li>
+                <li>
+                    <div className="companyname tagline estimateheader">
+                        {companydet.companyTagLine}
+
+                    </div>
+
+                </li>
+                <li>
+                    <div className="estimateheader">
+                        {companydet.companyAddress}
+                    </div>
+                </li>
+
+                <li>
+                    <div className="estimateheader">
+                        {companydet.companyPhno}
+                    </div>
+                </li>
+                <li>
+                    <div className="estimateheader">
+                        {companydet.companymailid}
+                    </div>
+                </li>
+            </ul>
+            {companydet.companyImage &&
+                <ul>
+                    <img src={companydet.companyImage} width="200" height="180" />
+                </ul>}
+
+            {/* <li>
+             {companydet.companyGstin ? 'Gstin: '+companydet.companyGstin:null }
             </li>
             <li>
-            Invoice date: {invoicedet.invoicedate}
-            </li>
-            <li>
-            Payment mode: {invoicedet.paymentmode}
-            </li>
-            <li>
-            Payment Date: {invoicedet.paymentdate}
-            </li>
-        </ul>
-    </div>
+             {companydet.companyGstinStatename ? 'StateName: '+companydet.companyGstinStatename:null }
+            </li> */}
+
+        </div>
+        
+        <div className="generaldetails">
+            <ul className="details">
+                <div className="companyname">Buyer:
+                    <li>
+                        {invoicedet.clientName}
+                    </li>
+                    <li>
+                        {invoicedet.clientPhno}
+                    </li>
+                    <li>
+                        {invoicedet.clientAdd}
+                    </li>
+                </div>
+            </ul>
+
+            <ul className="details">
+                <li className="companyname">
+                    {invoicedet.companyName}
+                </li>
+                <li>
+                    {invoicedet.companyTagLine}
+                </li>
+                <li>
+                    {invoicedet.companyAddress}
+                </li>
+                <li>
+                    {invoicedet.companyName}
+                </li>
+                <li>
+                    {invoicedet.companyPhno}
+                </li>
+                <li>
+                    {invoicedet.companyGstin ? 'Gstin: ' + invoicedet.companyGstin : null}
+                </li>
+                <li>
+                    {invoicedet.companyGstinStatename ? 'StateName: ' + invoicedet.companyGstinStatename : null}
+                </li>
+
+            </ul>
+
+            <ul className="details invoicedetails">
+
+                <li>
+                    Invoice ID: {invoicedet.invoiceid}
+                </li>
+                <li>
+                    Invoice date: {invoicedet.invoicedate}
+                </li>
+                <li>
+                    Payment mode: {invoicedet.paymentmode}
+                </li>
+                <li>
+                    Payment Date: {invoicedet.paymentdate}
+                </li>
+            </ul>
+        </div>
+    </>
 }
 
 export default GeneralDetails;
